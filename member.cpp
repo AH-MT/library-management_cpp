f1563a046ba5019e41820e78977b35055b2a77f2
Amir Hossein Moghbeli Tombanoei <amirhosainmoghblie8@gmail.com>
2026-08-04 21:35:04 +0330

    Add files via upload

--- member.cpp
+++ member.cpp
@@ -1,1 +1,29 @@
-#include <member.h>
+#include "member.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+void Member::borrowBook(){
+
+}
+void Member::returnBook(){
+
+}
+void Member::hasBorrowedBook(){
+
+}
+bool Member::canBorrow()const {
+
+}
+std::vector<std::string> Member::getBorrowedBooks()const{
+
+}
+int Member::getBorrowedCount()const {
+
+}
+void Member::displayInfo() const{
+    std::cout<<"name: "<<this->name<<"  |  id: "<<this->id;
+}
+std::string Member::getRole() const{
+    return "Member";
+}
