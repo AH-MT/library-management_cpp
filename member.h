85caa87ee4b708e637815300285987cb3ad0335c
Amir Hossein Moghbeli Tombanoei <amirhosainmoghblie8@gmail.com>
2026-08-04 23:35:34 +0330

    Update member.h

--- member.h
+++ member.h
@@ -2,21 +2,20 @@
 #include <string>
 #include <vector>
 #include "person.h"
+#include "book.cpp"
 
 class Member : public Person{
     private:
         std::vector<std::string> borrowedISBNs;
-    
-
     public:
         Member(std::string name, std::string id):Person(name,id){}
-        void borrowBook();
-        void returnBook();
+        void borrowBook(const Book* book);
+        void returnBook(const Book* book);
         void hasBorrowedBook();
         bool canBorrow()const;
         std::vector<std::string> getBorrowedBooks()const;
         int getBorrowedCount()const;
-
+        std::string getId()const;
         void displayInfo() const override;
         std::string getRole() const override;
 
