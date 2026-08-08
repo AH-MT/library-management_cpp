35be8888f1f7774f4230ec2c25a1647da15ee04d
Amir Hossein Moghbeli Tombanoei <amirhosainmoghblie8@gmail.com>
2026-08-08 21:38:46 +0330

    Update person.h

--- person.h
+++ person.h
@@ -5,16 +5,18 @@
 class Person{
     protected:
         std::string name;
-        const std::string id;
+        int id;
+        int password;
     public:
-        Person(std::string name, std::string id):name(std::move(name)),id(std::move(id)){}
+        Person(std::string name_t, int id_t, int pasword_t):name(std::move(name_t)),id(id_t),password(pasword_t){}
         //getters
         std::string getName()const;
-        std::string getId()const;
+        int getId()const;
+        int getPassword()const;
         virtual void displayInfo()const = 0;
         virtual std::string getRole()const = 0;
         
         //setters
-        void setName(const std::string& name);
+        void setName(const std::string& name_t);
         virtual ~Person(){}
 };
